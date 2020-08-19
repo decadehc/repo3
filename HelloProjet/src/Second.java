d0e20f72435b402094ef0b67cff559e538bb8248
hongcheng <863736681@qq.com>
2020-08-19 15:08:38 +0800

    新添加了一个类

---
+++ HelloProjet/src/Second.java
@@ -0,0 +1,12 @@
+public class Second {
+
+
+    public static void main(String[] args) {
+        System.out.println(" 添加了一个新的类的操作");
+
+        System.out.println("idea中的分支的操作");
+        System.out.println("idea中添加的操作");
+        System.out.println("nihao idea idea idea");
+    }
+
+}
